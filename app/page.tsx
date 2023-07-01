40dda55f8e2c843b2552583c0a4be8e81861541a
AndrzejSotnikov <sotnikow.dev@gmail.com>
2023-07-01 20:19:36 +0200

    add fetch rest on server side

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,44 @@
 import Header from "./components/Header/Header"
 import RestaurantCard from "./components/Card/Card"
+import { Cuisine, Location, PRICE, PrismaClient } from "@prisma/client"
 
-export default function Home() {
+const prisma = new PrismaClient()
+
+export interface RestaurantCardType {
+  id: number
+  name: string
+  main_image: string
+  cuisine: Cuisine
+  location: Location
+  price: PRICE
+}
+
+const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
+  const restaurants = await prisma.restaurant.findMany({
+    select: {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      location: true,
+      price: true,
+    },
+  })
+
+  return restaurants
+}
+
+export default async function Home() {
+  const restaurants = await fetchRestaurants()
+
   return (
-        <main>
-          <Header />
-          <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-            <RestaurantCard />
-          </div>
-        </main>
+    <main>
+      <Header />
+      <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
+        {restaurants.map((restaurant) => (
+          <RestaurantCard restaurant={restaurant} />
+        ))}
+      </div>
+    </main>
   )
 }
