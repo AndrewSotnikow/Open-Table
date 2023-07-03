621f0cdb60d9a00543ea69352fd71d0a611e8222
AndrzejSotnikov <sotnikow.dev@gmail.com>
2023-07-03 22:58:07 +0200

    render restaurant cards

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -53,21 +53,25 @@ const fetchRestaurantsByCity = (searchParams: SearchParams) => {
   })
 }
 
-const Search = ({
+const Search = async ({
   searchParams,
 }: {
   searchParams: {
     city: string
   }
 }) => {
-  const restaurants = fetchRestaurantsByCity(searchParams)
+  const restaurants = await fetchRestaurantsByCity(searchParams)
   return (
     <>
       <Header />
       <div className="flex py-4 m-auto w-2/3 justify-between items-start">
         <SearchSideBar />
         <div className="w-5/6">
-          <RestaurantCard />
+          {restaurants.length ? (
+            restaurants.map((rest) => <RestaurantCard restaurant={rest}/>)
+          ) : (
+            <p>Sorry, we found no restaurants in this area</p>
+          )}
         </div>
       </div>
     </>
