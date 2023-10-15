d72edeebb425a48c684a980687b4c2fb0db20526
AndrewSotnikow <sot.shot@gmail.com>
2023-10-15 14:48:20 +0200

    add AuthContext Component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import 'tailwindcss/tailwind.css'
 import NavBar from './components/NavBar/NavBar'
+import AuthContext from './context/AuthContext'
 
 export default function RootLayout({
     children,
@@ -12,10 +13,13 @@ export default function RootLayout({
             <head />
             <body className="text-black">
                 <main className="bg-gray-100 min-h-screen w-screen">
-                    <main className="max-w-screen-2xl m-auto bg-white">
-                        <NavBar />
-                        {children}
-                    </main>
+                    <AuthContext>
+                        <main className="max-w-screen-2xl m-auto bg-white">
+                            <NavBar />
+                            {children}
+                        </main>
+                    </AuthContext>
+
                 </main>
             </body>
         </html>
